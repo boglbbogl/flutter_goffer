50852d96e22bd90a9977fb1b18dde25b80fc0fa6
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2021-10-20 16:10:39 +0900

    프로젝트 초기 enviroment 구축 작업 완료

--- lib/constant/flavor/config_reader.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:convert';
-
-import 'package:flutter/services.dart';
-
-abstract class ConfigReader {
-  static Map<String, dynamic>? _config;
-
-  static Future<void> initialize() async {}
-}
--- lib/constant/flavor/main_common.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-Future<void> mainCommon(String env) async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // await ConfigReader
-}
